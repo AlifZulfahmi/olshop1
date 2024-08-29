80b3eb23ef8a5bbef753a1f8857ff73afa8dc34c
Alif Zulfahmi Yusuf <alifyusuf200@gmail.com>
2024-08-29 22:54:47 +0700

    eta kop

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,38 +1,52 @@
-@extends('layouts.app')
+@extends('layouts.adminApp')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2> Show User</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                {{ $user->name }}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Email:</strong>
-                {{ $user->email }}
+    <div class="container">
+        <div class="page-inner">
+            <div class="page-header">
+                <h3 class="fw-bold mb-3">Profile</h3>
+                <ul class="breadcrumbs mb-3">
+                    <li class="nav-home">
+                        <a href="#">
+                            <i class="icon-home"></i>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="icon-arrow-right"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">My Profile</a>
+                    </li>
+                </ul>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Roles:</strong>
-                @if (!empty($user->getRoleNames()))
-                    @foreach ($user->getRoleNames() as $v)
-                        <label class="badge badge-success">{{ $v }}</label>
-                    @endforeach
-                @endif
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card card-profile">
+                    <div class="card-header" style="background-image: url('assets/img/blogpost.jpg')">
+                        <div class="profile-picture">
+                            <div class="avatar avatar-xl">
+                                <img src="assets/img/profile.jpg" alt="..." class="avatar-img rounded-circle" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="user-profile text-center">
+                            <div class="name">{{ $user->name }}</div>
+                            <div class="job">{{ $user->email }}</div>
+                            <div class="desc">
+                                @if (!empty($user->getRoleNames()))
+                                    @foreach ($user->getRoleNames() as $v)
+                                        <label class="badge badge-success">{{ $v }}</label>
+                                    @endforeach
+                                @endif
+                            </div>
+                            <div class="view-profile">
+                                <a href="{{ route('users.index') }}" class="btn btn-secondary w-100">Back</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
